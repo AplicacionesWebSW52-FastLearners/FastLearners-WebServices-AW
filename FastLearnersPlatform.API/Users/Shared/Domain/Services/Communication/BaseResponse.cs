0e2168c0b4ad6d28c79f61308d229ae879ee7362
Marco Mendez <u202018273@upc.edu.pe>
2024-06-11 22:34:31 -0500

    Created BaseResponse

---
+++ FastLearnersPlatform.API/Users/Shared/Domain/Services/Communication/BaseResponse.cs
@@ -0,0 +1,18 @@
+namespace FastLearnersPlatform.API.Users.Shared.Domain.Services.Communication;
+
+public class BaseResponse<T>
+{
+    protected BaseResponse(string message)
+    {
+        Message = message;
+    }
+
+    protected BaseResponse(T resource)
+    {
+        Resource = resource;
+    }
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public T Resource { get; set; }
+    
+}
